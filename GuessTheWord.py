175079214f49d12fda7077d37cdbc2ae416c75ad
CharithShettigar <81745164+CharithShettigar@users.noreply.github.com>
2021-11-20 19:35:48 +0530

    Add files via upload

---
+++ GuessTheWord.py
@@ -0,0 +1,23 @@
+print("-------------------------------------------------------------------------")
+print("                   WELCOME TO WORD GUESSING GAME")
+print("-------------------------------------------------------------------------")
+wordcollection=['Hello','name','dragon','class','tree','beautiful','object','anime','fish','tiger','lady','ant','man','world']
+
+name=input("Enter username:")
+flag=0
+
+while(flag==0):
+    word=input("Enter your word that your guessing:")
+    for i in wordcollection:
+        if word.upper()==i.upper():
+            print(f"{name} guessed the correct word!!! \n CONGRATULATIONS\nGame is ENDING....")
+            flag=1
+    if(flag==0):
+        print("You guessed the wrong word!!!")
+        
+        
+        
+# print(i,word)
+# if word.upper()==i.upper():
+#     print("You guessed the correct word!!!")
+
